a54e3c63cf06b984de17070b436996c5b1f46920
MOIS3Y <stepan@zhukovsky.me>
2024-03-23 16:58:38 +0900

    Update: flake.nix poetry2nix is now maintained out-of-tree nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,22 +4,43 @@
     with the ability to change the color scheme and some parameters'';
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, poetry2nix, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv;
     in {
-    packages.${system}.i3lock-color-wrapper = with pkgs;
-      poetry2nix.mkPoetryApplication {
-        projectDir = self;
+
+    # Executed by `nix build .#<name>
+    # ? Example: nix build .#i3lock-color-wrapper 
+    packages.${system} = {
+      i3lock-color-wrapper = with pkgs; mkPoetryApplication { 
+        projectDir = self; 
         preferWheels = true;
         propagatedBuildInputs = [
           i3lock-color
         ];
       };
+      default = self.packages.${system}.i3lock-color-wrapper;
+    };
+    
+    # Executed by `nix run . -- <args?>`
+    # ? example: nix run .#i3lock-color-wrapper -- --version
+    apps.${system}.i3lock-color-wrapper = {
+      type = "app";
+      program = "${self.packages.${system}.i3lock-color-wrapper}/bin/i3lock-run";
+    };
+
+    # Used by `nix develop`
+    # ? Example: nix develop .#i3lock-color-wrapper
     devShells.${system}.i3lock-color-wrapper = pkgs.mkShellNoCC {
       shellHook = ''
         echo
@@ -29,7 +50,7 @@
         echo
       '';
       packages = with pkgs; [
-        (poetry2nix.mkPoetryEnv { projectDir = self; preferWheels = true; })
+        (mkPoetryEnv { projectDir = self; preferWheels = true; })
         i3lock-color
       ];
     };
